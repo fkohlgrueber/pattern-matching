a98e007e6c5be063cac2c007d3aafa78b904e0ae
Felix Kohlgrüber <felix.kohlgrueber@gmail.com>
2019-03-01 08:47:27 +0100

    refactoring

--- pattern-match/src/matchers.rs
+++ pattern-match/src/matchers.rs
@@ -6,6 +6,29 @@ pub struct RepeatRange {
     pub end: Option<usize>  // exclusive
 }
 
+impl RepeatRange {
+    fn mul(&self, other: &RepeatRange) -> RepeatRange {
+        RepeatRange {
+            start: self.start * other.start,
+            end: self.end.and_then(|s| other.end.map(|o| s * o))
+        }
+    }
+
+    fn add(&self, other: &RepeatRange) -> RepeatRange {
+        RepeatRange {
+            start: self.start + other.start,
+            end: self.end.and_then(|s| other.end.map(|o| s + o))
+        }
+    }
+
+    fn max_range(&self, other: &RepeatRange) -> RepeatRange {
+        RepeatRange {
+            start: std::cmp::min(self.start, other.start),
+            end: self.end.and_then(|s| other.end.map(|o| std::cmp::max(s, o)))
+        }
+    }
+}
+
 type Setter<'cx, 'o, Cx, O> = fn(&'cx mut Cx, &'o O) -> &'cx mut Cx;
 
 #[derive(Debug)]
@@ -37,6 +60,8 @@ pub enum Opt<'cx, 'o, T, Cx, O> {
 }
 
 impl<'cx, 'o, T, Cx, O> Seq<'cx, 'o, T, Cx, O> {
+
+    /// Returns the min / max repetitions that can match the Seq
     pub fn num_elmts_range(&self) -> RepeatRange {
         match self {
             Seq::Any => RepeatRange { start: 1, end: Some(2)},
@@ -44,35 +69,17 @@ impl<'cx, 'o, T, Cx, O> Seq<'cx, 'o, T, Cx, O> {
             Seq::Elmt(_) => RepeatRange { start: 1, end: Some(2)},
             Seq::Repeat(e, r) => {
                 let e_range = e.num_elmts_range();
-                RepeatRange {
-                    start: e_range.start * r.start,
-                    end: match (e_range.end, r.end) {
-                        (Some(a), Some(b)) => Some(a*b),
-                        _ => None
-                    }
-                }
+                e_range.mul(r)
             },
             Seq::Seq(a, b) => {
                 let a_range = a.num_elmts_range();
                 let b_range = b.num_elmts_range();
-                RepeatRange {
-                    start: a_range.start + b_range.start,
-                    end: match (a_range.end, b_range.end) {
-                        (Some(a), Some(b)) => Some(a+b),
-                        _ => None
-                    }
-                }
+                a_range.add(&b_range)
             },
             Seq::Alt(a, b) => {
                 let a_range = a.num_elmts_range();
                 let b_range = b.num_elmts_range();
-                RepeatRange {
-                    start: std::cmp::min(a_range.start, b_range.start),
-                    end: match (a_range.end, b_range.end) {
-                        (Some(a), Some(b)) => Some(std::cmp::max(a, b)),
-                        _ => None
-                    }
-                }
+                a_range.max_range(&b_range)
             },
             Seq::Named(e, _) => {
                 e.num_elmts_range()
